286a5bece80cc0145f59597567d95820544b4c44
Prashant Rajoriya <prashant.rajoriya21@gmail.com>
2018-09-13 15:40:53 +0530

    Add Legend and ToolTip

--- main.js
+++ main.js
@@ -4,27 +4,44 @@ d3.json(url, function(error, data) {
   if (error) throw error;
 
 	let baseTemperature = data.baseTemperature,
-			monthlyVariance = data.monthlyVariance;
+			monthlyVariance = data.monthlyVariance,
+			varianceRange   = [];
 
 	monthlyVariance.map(d => {
 		d.variance = d.variance + baseTemperature
 		return d.variance;
 	})
 
+	let minMaxTemp = d3.extent(monthlyVariance, d => d.variance);
+
+	function CalculateVariance(min, max, count){
+		let arr = [];
+		let step = (max - min)/count;
+		for (let index = 1; index <= count; index++) {
+			arr.push(index*step);
+		}
+		return arr;
+	}
+
 	let width = 1518,
 			height = 600,
 		  cellWidth = Math.ceil(width/Math.ceil(monthlyVariance.length/12)),
 			cellHeight = Math.ceil(height/12),
-			margin = { top: 120, right: 10, bottom: 120, left: 100 };
+			margin = { top: 120, right: 60, bottom: 120, left: 100 };
 
 	let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 	'August', 'September', 'October', 'November', 'December'];
-
-			
+		
 	let svg = d3.select('.container')
 							.append('svg')
 							.attr('width', width + margin.left + margin.right)
 							.attr('height', height + margin.top + margin.bottom);
+						
+	let toolTip = d3.select('.container')
+									.append('div')
+									.classed('tooltip',true)
+									.attr('id', 'tooltip')
+									.style('opacity', 0);
 
 	let xScale = d3.scaleLinear()
 									.domain(d3.extent(monthlyVariance, d => d.year))
@@ -43,6 +60,19 @@ d3.json(url, function(error, data) {
 
 	let xAxis = d3.axisBottom(xScale)
 								.tickFormat(d3.format('d'));
+
+	let legendHeight = 100,
+			legendWidth = 400,
+			colors = cScale.range();
+
+	varianceRange = CalculateVariance(minMaxTemp[0], minMaxTemp[1], colors.length);	
+	
+	let lScale = d3.scaleBand()
+									.domain(varianceRange)
+									.range([0,legendWidth]);
+
+	let lAxis = d3.axisBottom(lScale)
+								.tickFormat(d3.format('.1f'));
 
 	svg
 		.append('g')
@@ -69,7 +99,27 @@ d3.json(url, function(error, data) {
 		.attr('width', cellWidth)
 		.attr('height', cellHeight)
 		.attr('fill', d => cScale(d.variance))
-		.attr('transform', `translate(${margin.left}, ${margin.top})`);
+		.attr('transform', `translate(${margin.left}, ${margin.top})`)
+		.on('mouseover', (d, i) => {
+			toolTip
+				.attr('data-year', d.year)
+				.transition()
+				.duration(200);
+
+			toolTip	
+				.style('opacity', '1')
+				.style('left', d3.event.x+10 + 'px')
+				.style('top', d3.event.y + 'px')
+				.html(`
+					<p>${d.year} - ${months[d.month]}</p>
+					<p>${d3.format('.1f')(d.variance)} &#8451;</p>
+				`);
+			
+		})
+		.on('mouseout', (d, i) => {
+			toolTip
+				.style('opacity', 0);
+		})
 
 	svg
 		.append('text')
@@ -88,5 +138,31 @@ d3.json(url, function(error, data) {
 		.style('font-size', '1.5em')
 		.style('text-anchor', 'middle')
 		.text('1753 - 2015: base temperature 8.66℃');
+
+	let legend = svg
+							.append('g')
+							.attr('transform', `translate(${margin.left+30}, ${margin.top+height+margin.bottom-70})`)
+							.attr('id', 'legend')
+							.attr('width', legendWidth)
+							.attr('height', legendHeight);
+
+	legend
+		.selectAll('rect')
+		.data(varianceRange)
+		.enter()
+		.append('rect')
+		.attr('x', (d, i) => 40*i)
+		.attr('y', 0)
+		.attr('width', 40)
+		.attr('height', 30)
+		.attr('fill', d => cScale(d))
+		.style('stroke', 'black')
+		.style('stroke-width', 2);
+
+	legend	
+		.append('g')
+		.attr('transform', `translate(0, ${30})`)
+		.call(lAxis);
+	
 
 })
